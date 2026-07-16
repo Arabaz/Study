4464c86c0c30f0b3dcd280d227b511de23dc504f
Arabaz Ahamad <arabazahamad68@gmail.com>
2026-07-16 23:48:29 +0530

    Defer keyword
    Defer keyword

--- InterviewPrep.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ InterviewPrep.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -43,3 +43,58 @@ print(flat)
  How do we solve it?
  Use weak or unowned.
  */
+
+
+
+//MARK: defer
+/*
+ The defer keyword in Swift is used to delay the execution of a block of code until the current scope exits.
+
+ Think of it as a cleanup block. No matter how the function exits (normal return, error, or early return), the code inside defer will always execute before leaving the scope.
+
+ Real-Life Example
+
+ Imagine you're using a meeting room.
+
+ Enter the room.
+ Lock the door.
+ Finish your work.
+ Unlock the door before leaving.
+
+ Instead of remembering to unlock the door in every possible exit point, you can simply say:
+
+ "When I'm about to leave, unlock the door."
+
+ That's exactly what defer does.
+ */
+
+func test() -> String {
+    defer {
+        print("Goodbye")
+    }
+
+    print("Hello")
+    return "Done"
+}
+
+print(test())
+
+func test1() -> String {
+    defer {
+        print("Goodbye3")
+    }
+    defer {
+        print("Goodbye")
+    }
+    defer {
+        print("Goodbye1")
+    }
+    defer {
+        print("Goodbye2")
+    }
+
+    print("Hello")
+    return "Done"
+}
+
+print(test1())
