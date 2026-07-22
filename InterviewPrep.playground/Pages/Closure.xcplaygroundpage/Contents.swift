8b8e9765e6a0dcc0082cc70f8f9b4046026b457f
Arabaz Ahamad <arabazahamad68@gmail.com>
2026-07-22 09:07:23 +0530

    Closure with types
    Closure with types

--- InterviewPrep.playground/Pages/Closure.xcplaygroundpage/Contents.swift
+++ InterviewPrep.playground/Pages/Closure.xcplaygroundpage/Contents.swift
@@ -27,3 +27,51 @@ import Foundation
 
  Notice there is no function name.
  */
+// Closure with Parameters
+let sum = { (a: Int, b: Int) -> Int in
+    return a + b
+}
+
+print(sum(10, 5))
+
+//Closure with No Parameters
+let welcome = {
+    print("Welcome")
+}
+print(welcome)
+
+//Closure Returning Value
+let square = { (number: Int) -> Int in
+    return number * number
+}
+print(square(6))
+
+//MARK: Trailing Closure
+/*When the last parameter of a function is a closure, Swift allows us to write that closure outside the parentheses.*/
+
+
+
+
+//Escaping Closure
+
+func fetchData(completion: @escaping () -> Void) {
+    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+        completion()
+    }
+}
+
+fetchData {
+    print("Data l   `oaded")
+}
+
+//Non-Escaping Closure
+//Autoclosure
+//Capture Values
+
+//Strong Capture, Weak Capture, Unowned Capture
+
+//Retain Cycle Example
+
+//Completion Handler
+
+
