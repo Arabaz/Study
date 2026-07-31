3643972ef1e34276c16675bf1a1da0cf528c6103
Arabaz Ahamad <arabazahamad68@gmail.com>
2026-07-31 09:41:33 +0530

    Practice
    Practice

--- InterviewPrep.playground/Pages/Practice.xcplaygroundpage/Contents.swift
+++ InterviewPrep.playground/Pages/Practice.xcplaygroundpage/Contents.swift
@@ -2,12 +2,6 @@
 
 import Foundation
 
-var greeting = "Hello, playground"
-
-//: [Next](@next)
-
-print(greeting)
-
 //MARK: Find two numbers whose sum equals the target.
 
 let Input = [2,7,11,15]
@@ -157,3 +151,36 @@ while left < right {
     right -= 1
 }
 print(isPalindrome)
+
+//MARK: Next Greater Element ⭐⭐⭐⭐⭐
+var arr1 = [10, 4, 2, 20, 40, 12, 30]//[2, 5, 1, 7]//[2, 2, 2]//[5]//[4, 3, 2, 1]//[1, 2, 3, 4]//[13, 7, 6, 12]//[4, 5, 2, 25]//[2,1,3]
+//Ip: 2 1 3 , op: 3 3 -1
+
+var result = Array(repeating: -1, count: arr1.count)
+
+for i in 0..<arr1.count{
+    for j in (i + 1)..<arr1.count{
+        if arr1[j] > arr1[i]{
+            result[i] = arr1[j]
+            break
+        }
+    }
+}
+print(result)
+
+var result1 = Array(repeating: -1, count: arr1.count)
+var stack = [Int]()
+
+for i in stride(from: arr1.count - 1, through: 0, by: -1){
+    
+    while let last = stack.last, last <= arr1[i] {
+        stack.removeLast()
+    }
+    
+    if let last = stack.last {
+        result1[i] = last
+    }
+            
+    stack.append(arr1[i])
+}
+print(result1)
